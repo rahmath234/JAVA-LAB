eb6784ba1a822dcb11c5a064a627428e4b45809b
rahmath234 <rahmathck381@gmail.com>
2026-02-25 11:49:09 +0530

    Add files via upload

---
+++ SymmetricMatrix.java
@@ -0,0 +1,33 @@
+package rahmath;
+import java.util.Scanner;
+public class SymmetricMatrix {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n;
+		boolean symmetric=true;
+		System.out.print("Enter order of matrix:");
+		n=sc.nextInt();
+		int[][]A=new int[n][n];
+		System.out.println("Enter matrix elements:");
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++) {
+				A[i][j]=sc.nextInt();
+			}
+		}		
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++) {
+				if(A[i][j] != A[j][i]) {
+					symmetric=false;
+					break;
+				}
+			}
+	}
+		if(symmetric) {
+			System.out.println("The matrix is Symmetic.");
+		}else {
+			System.out.println("The matrix is NOT Symmetic.");
+		}
+		sc.close();
+
+    }
+}
