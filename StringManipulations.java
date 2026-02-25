1b13f2c23c8d527c0890100cdda99fcc671744de
rahmath234 <rahmathck381@gmail.com>
2026-02-25 11:40:34 +0530

    Add files via upload

---
+++ StringManipulations.java
@@ -0,0 +1,35 @@
+package rahmath;
+
+import java.util.Scanner;
+
+public class StringManipulations {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a string:");
+		String str = sc.nextLine();
+		System.out.println("Length:" +str.length());
+		System.out.println("Uppercase:" +str.toUpperCase());
+		System.out.println("Lowercase:" +str.toLowerCase());
+		String reversed = "";
+		for(int i = str.length()-1; i >= 0; i--)
+		{
+			reversed += str.charAt(i);
+		}
+		System.out.println("Reversed String:" + reversed);
+		System.out.print("Enter another string to concatenate:");
+		String str2 = sc.nextLine();
+		System.out.println("concatenated string:" + str.concat(str2));
+		if(str.equals(str2))
+		{
+			System.out.println("Both strings are equal.");
+		}else
+		{
+			System.out.println("Strings are not equal.");
+		}
+		sc.close();
+		
+	}
+
+}
